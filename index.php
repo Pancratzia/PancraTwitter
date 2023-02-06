7e1aee7e974007506960a7fc98af12fd04a0bf29
Pancratzia <ortegalaura180999@gmail.com>
2023-02-06 15:03:50 -0400

    Inicio del Proyecto Pancratwitter: Clon de Twitter

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PancraTwitter</title>
+</head>
+<body>
+
+    <section class="main-page">
+        <div class="left">Left</div>
+        <div class="right">Right</div>
+        <footer aria-label="footer" class="main-page-footer">Footer</footer>
+    </section>
+    
+</body>
+</html>
