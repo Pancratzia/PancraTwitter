4b5d43dcf4e4926707487d12bb27e1d822594b2d
Pancratzia <ortegalaura180999@gmail.com>
2023-02-21 22:42:43 -0400

    Se agrega composser y todos los datos delicados pasan a ser parte de las variables de entorno

--- backend/config.php
+++ backend/config.php
@@ -1,14 +1,26 @@
 <?php
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'pancra');
-define('DB_PASSWORD', 'password');
-define('DB_NAME', 'pancratwitter');
+require 'vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+define('DB_HOST', $_ENV['DB_HOST']);
+define('DB_USER', $_ENV['DB_USER']);
+define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
+define('DB_NAME', $_ENV['DB_NAME']);
+
+define("M_HOST", $_ENV['M_HOST']);
+define("M_USER", $_ENV['M_USER']);
+define("M_PASSWORD", $_ENV['M_PASSWORD']);
+define("M_SMTPSECURE", $_ENV['M_SMTPSECURE']);
+define("M_PORT", $_ENV['M_PORT']);
 
 $public_end=strpos($_SERVER['SCRIPT_NAME'],'/frontend')+8;
 $doc_root=substr($_SERVER['SCRIPT_NAME'],0,$public_end);
 
 define("WWW_ROOT",$doc_root);
+
+
 
 
 ?>
--- backend/initialize.php
+++ backend/initialize.php
@@ -5,6 +5,9 @@ ob_start();
 date_default_timezone_set('America/Caracas');
 
 require_once('backend/config.php');
+include("classes/PHPMailer.php");
+include("classes/SMTP.php");
+include("classes/Exception.php");
 
 spl_autoload_register(function ($class) {
     require_once "classes/{$class}.php";
